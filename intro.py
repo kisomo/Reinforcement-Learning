521a51b904a903f546711304a21b76a3b390cb89
kisomo <tmuthoka@gmail.com>
2018-04-15 12:22:52 -0500

    first commit

---
+++ intro.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+#https://hackernoon.com/the-self-learning-quant-d3329fcc9915
+
+def load_data():
+    price = np.arange(200/10.0) #linearly increasing prices
+    return price
+
+x = load_data()
+print(x)
+
+epochs = 5
+
+for i in range(epochs):
+
+    state, xdata = init_state(csv)
+    status = 1
+    terminal_state = 0
+    time_step = 1
+    #while learning is still in progress
+    while(status == 1):
+        #We start in state S
+        #Run the Q function on S to get predicted reward values on all the possible actions
+        qval = model.predict(state.reshape(1,2), batch_size=1)
+        if (random.random() < epsilon) and i != epochs - 1: #choose random action
+            action = np.random.randint(0,4) #assumes 4 different actions
+        else: #choose best action from Q(s,a) values
+            action = (np.argmax(qval))
+        #Take action, observe new state S'
+        new_state, time_step, signal, terminal_state = take_action(state, xdata, action, signal, time_step)
+        #Observe reward
+        reward = get_reward(new_state, time_step, action, xdata, signal, terminal_state, i)
+        #Get max_Q(S',a)
+        newQ = model.predict(new_state.reshape(1,2), batch_size=1)
+        maxQ = np.max(newQ)
+        y = np.zeros((1,4))
+        y[:] = qval[:]
+        if terminal_state == 0: #non-terminal state
+            update = (reward + (gamma * maxQ))
+        else: #terminal state (means that it is the last state)
+            update = reward
+        y[0][action] = update #target output
+        model.fit(state.reshape(1,2), y, batch_size=1, nb_epoch=1, verbose=0)
+        state = new_state
+        if terminal_state == 1: #terminal state
+            status = 0
+    print("Epoch #: %s Reward: %f Epsilon: %f" % (i,reward, epsilon))
+    learning_progress.append((reward))
+    if epsilon > 0.1:
+        epsilon -= (1.0/epochs)
+
+
+'''
+#This neural network is the the Q-function
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import RMSprop
+
+model = Sequential()
+model.add(Dense(4, init='lecun_uniform', input_shape=(2,)))
+model.add(Activation('relu'))
+
+model.add(Dense(4, init='lecun_uniform'))
+model.add(Activation('relu'))
+
+model.add(Dense(4, init='lecun_uniform'))
+model.add(Activation('linear')) #linear output so we can have range of real-valued outputs
+
+rms = RMSprop()
+model.compile(loss='mse', optimizer=rms)
+
+
+#from the main loop above
+if terminal_state == 0: #non-terminal state
+  update = (reward + (gamma * maxQ))
+else: #terminal state (means that it is the last state)
+  update = reward
+
+
+def get_reward(new_state, time_step, action, xdata, signal, terminal_state, epoch=0):
+    reward = 0
+    signal.fillna(value=0, inplace=True)
+    if terminal_state == 0:
+        #get reward for the most current action
+        if signal[time_step] != signal[time_step-1] and terminal_state == 0:
+            i=1
+            while signal[time_step-i] == signal[time_step-1-i] and time_step - 1 - i > 0:
+                i += 1
+            reward = (xdata[time_step-1, 0] - xdata[time_step - i-1, 0]) * signal[time_step - 1]*-100 + i*np.abs(signal[time_step - 1])/10.0
+        if signal[time_step] == 0 and signal[time_step - 1] == 0:
+            reward -= 10
+
+    #calculate the reward for all actions if the last iteration in set
+    if terminal_state == 1:
+        #run backtest, send list of trade signals and asset data to backtest function
+        bt = twp.Backtest(pd.Series(data=[x[0] for x in xdata]), signal)
+        reward = bt.pnl.iloc[-1]
+
+    return reward
+
+
+'''
